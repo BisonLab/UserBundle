e85edb69d7271438c6008cc96f5bd0747233840f
Thomas Lundquist <thomasez@bisonlab.no>
2026-09-23 22:29:50 +0200

    No more Annotations.

--- Controller/GroupController.php
+++ Controller/GroupController.php
@@ -8,7 +8,7 @@ use BisonLab\UserBundle\Repository\GroupRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
 #[Route(path: '/bisonlab_group')]
--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -7,7 +7,7 @@ use BisonLab\UserBundle\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
